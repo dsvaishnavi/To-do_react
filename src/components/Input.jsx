ca65c27ad5dff5f19dae78392b902be124664cbd
dsvaishnavi <vaishnavidhawade1@gmail.com>
2024-12-30 15:33:00 +0530

    make components

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+export const Input=()=>{
+    return(
+        <>
+        <div>
+            <div>
+                <input type="text" />
+            </div>
+        </div>
+        </>
+    )
+};
